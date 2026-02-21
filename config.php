347ef618ee6cb698db6d0d44cb65c51742a7752f
DeepakWorld <deepak.thangaswamy@gmail.com>
2026-02-21 14:28:24 +0530

    Remove functions property to fix api directory error

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'db_host'     => 'qlxxtoozjdxxcrrvmcon.pooler.supabase.com',
+    'db_host'     => 'db.qlxxtoozjdxxcrrvmcon.supabase.co', // Direct Host
     'db_port'     => '5432',
     'db_database' => 'postgres',
-    'db_username' => 'postgres.qlxxtoozjdxxcrrvmcon', // Note: Supabase Pooler often requires 'user.project-id'
+    'db_username' => 'postgres', // Use 'postgres' for direct connections
     'db_password' => 'Litmus@123d',
 ];
