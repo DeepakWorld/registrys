3d9e85ede7bb143ffc8798d8efd3c58a0258f422
DeepakWorld <deepak.thangaswamy@gmail.com>
2026-02-21 15:37:35 +0530

    Remove functions property to fix api directory error

--- config.php
+++ config.php
@@ -3,6 +3,6 @@ return [
     'db_host'     => 'qlxxtoozjdxxcrrvmcon.supabase.co', // Direct Host
     'db_port'     => '5432',
     'db_database' => 'postgres',
-    'db_username' => 'postgres', // Use 'postgres' for direct connections
+    'db_username' => 'postgres.qlxxtoozjdxxcrrvmcon', // Use 'postgres' for direct connections
     'db_password' => 'Litmus@123d',
 ];
